c720da8a2c6dd4f03ddbce370e9c26ed6046749b
Saiken Ho <saikenhh@gmail.com>
2025-05-22 22:27:12 -0400

    added post controller

--- api/controllers/post.controller.jsx
+++ api/controllers/post.controller.jsx
@@ -2,19 +2,21 @@ import prisma from "../lib/prisma";
 
 export const getPosts = async (req, res) => {
   try {
-    res.status(200).json();
+    const posts = await prisma.post.findMany();
+
+    res.status(200).json(posts);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts " });
   }
 };
 
-
-
-
 export const getPost = async (req, res) => {
+  const id = req.params;
   try {
-    res.status(200).json();
+    const post = await prisma.post.findUnique({ where: { id } });
+
+    res.status(200).json(post);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts " });
@@ -22,8 +24,17 @@ export const getPost = async (req, res) => {
 };
 
 export const addPost = async (req, res) => {
+  const body = req.body;
+  const tokenUserId = req.userId;
+
   try {
-    res.status(200).json();
+    const newPost = await prisma.post.create({
+      data: {
+        ...body,
+        userId: tokenUserId,
+      },
+    });
+    res.status(200).json(newPost);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts " });
@@ -39,11 +50,21 @@ export const updatePost = async (req, res) => {
   }
 };
 export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
   try {
-    res.status(200).json();
+    const post = await prisma.post.findUnique({
+      where: { id },
+    });
+    if (post.userId !== tokenUserId) {
+      return res.status(403).json({ message: "Not authorized " });
+    }
+    await prisma.post.delete({
+      where: { id },
+    });
+    res.status(200).json({message:"Post delete"});
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts " });
   }
 };
-
